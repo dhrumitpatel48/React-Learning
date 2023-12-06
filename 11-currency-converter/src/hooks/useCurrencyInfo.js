2fb866484bdd7134c9f9f637f4517135b9ca52f4
Dhrumit Patel <dhrumitpatel48@gmail.com>
2023-12-06 22:07:04 +0530

    Completed currency converter

--- 11-currency-converter/src/hooks/useCurrencyInfo.js
+++ 11-currency-converter/src/hooks/useCurrencyInfo.js
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
+  console.log(currency)
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
       .then((res) => res.json())
-      .then((res) => setData(res));
+      .then((res) => setData(res[currency]));
     console.log(data);
-  }, []);
+  }, [currency]);
   return data
 }
 
